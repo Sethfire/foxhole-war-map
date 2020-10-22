39f866989ad2a1f6773139a15bf31fbe64f82a5a
Sethfire <sethfire404@gmail.com>
2020-10-22 22:12:56 +0800

    Implemented location names

--- src/Components/Map/MapItems.js
+++ src/Components/Map/MapItems.js
@@ -4,13 +4,8 @@ import {Marker, LayerGroup, Tooltip} from 'react-leaflet';
 import {o, w, k, mapArray} from './MapData.js';
 
 import MapItem from './MapItem.js';
+import MapTextItem from './MapTextItem.js';
 import * as MapIcon from './IconData.js';
-
-function convertCoords(regionId,x,y) {
-    var xcoord = mapArray[regionId-3].center[1] - (w/2) + (w*x);
-    var ycoord = mapArray[regionId-3].center[0] + (k/2) - (k*y);
-    return {xcoord,ycoord};
-}
 
 class MapItems extends React.Component{
 
@@ -19,23 +14,57 @@ class MapItems extends React.Component{
         this.state = {
             staticLoaded: false,
             dynamicLoaded: false,
-            mapMarkers: [],
+            mapItems: [],
             mapTextItems: []
         }
     }
 
+    convertCoords(regionId, x, y) {
+        let xcoord = mapArray[regionId-3].center[1] - (w/2) + (w*x);
+        let ycoord = mapArray[regionId-3].center[0] + (k/2) - (k*y);
+        return {xcoord,ycoord};
+    }
+
+    compare(a, b) {
+        if (a.distance < b.distance) {
+            return -1;
+        }
+        if (a.distance > b.distance) {
+            return 1;
+        }
+        return 0;
+    }
+
+    findClosest(mapItem) {
+        let closestNames = [];
+    
+        this.state.mapTextItems.map(mapTextItem => {
+            if (mapItem.regionId === mapTextItem.regionId) {
+                let xdif = Math.abs(mapItem.x - mapTextItem.x);
+                let ydif = Math.abs(mapItem.y - mapTextItem.y);
+                let distance = Math.sqrt(Math.pow(xdif, 2) + Math.pow(ydif, 2));
+                closestNames.push({text: mapTextItem.text, distance: distance});
+            }
+        });
+    
+        //console.log(mapItem.regionId, closestNames);
+        closestNames.sort(this.compare);
+        return closestNames[0].text;
+    }
+
     componentDidMount() {
 
-        /*
-        fetch(process.env.PUBLIC_URL + 'test-data-static.json')
+        fetch(process.env.PUBLIC_URL + '/test-data-static.json')
         .then(response => response.json())
         .then(data => {
             data.forEach(region => {
-                if(region == null) { return; }
+                if(region === null) { return; }
                 region.mapTextItems.map(mapTextItem => {
+                    let coords = this.convertCoords(region.regionId, mapTextItem.x, mapTextItem.y);
+                    let mapTextItemObject = new MapTextItem(region.regionId,mapTextItem.text,coords.xcoord,coords.ycoord);
                     this.setState(prevState => {
-                        const MapTextItems = prevState.MapTextItems.concat({});
-                        return { MapTextItems };
+                        const mapTextItems = prevState.mapTextItems.concat(mapTextItemObject);
+                        return { mapTextItems };
                     });
                 });
             });
@@ -43,20 +72,19 @@ class MapItems extends React.Component{
                 staticLoaded: true
             });
         });
-        */
 
-        fetch(process.env.PUBLIC_URL + 'test-data-dynamic.json')
+        fetch(process.env.PUBLIC_URL + '/test-data-dynamic.json')
         .then(response => response.json())
         .then(data => {
             data.forEach(region => {
-                if(region == null) { return; }
+                if(region === null) { return; }
                 region.mapItems.map(mapItem => {
-                    let coords = convertCoords(region.regionId, mapItem.x, mapItem.y);
+                    let coords = this.convertCoords(region.regionId, mapItem.x, mapItem.y);
                     let mapItemObject = new MapItem(region.regionId,mapItem.teamId,mapItem.iconType,coords.xcoord,coords.ycoord,mapItem.flags);
                     if (mapItemObject.iconImage != null) {
                         this.setState(prevState => {
-                            const mapMarkers = prevState.mapMarkers.concat(mapItemObject);
-                            return { mapMarkers };
+                            const mapItems = prevState.mapItems.concat(mapItemObject);
+                            return { mapItems };
                         });
                     }
                 });
@@ -68,12 +96,11 @@ class MapItems extends React.Component{
     }
 
     render() {
-        if (this.state.dynamicLoaded) {
-
-            const mapMarkers = this.state.mapMarkers.map(mapItem => {
+        if (this.state.staticLoaded && this.state.dynamicLoaded) {
+            const mapMarkers = this.state.mapItems.map(mapItem => {
                 return(
                     <Marker icon={mapItem.iconImage} position={[mapItem.y,mapItem.x]}>
-                        <Tooltip sticky><strong><font color='#d67b52'>{mapItem.teamPrefix}{mapItem.description}</font></strong><br />{mapItem.regionName} </Tooltip> 
+                        <Tooltip sticky><strong><font color='#d67b52'>{this.findClosest(mapItem)}</font></strong><br />{mapItem.teamPrefix}{mapItem.description}<br />{mapItem.regionName}</Tooltip> 
                     </Marker> 
                 );
             });
